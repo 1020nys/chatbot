a2aaa9040dd55b72a3c32e3c2abdd4da1ab8940f
yeonsoo Noh <122529151+1020nys@users.noreply.github.com>
2025-04-30 16:16:02 +0900

    Update st_function.py

--- rag_bk/st_function.py
+++ rag_bk/st_function.py
@@ -17,10 +17,20 @@ def print_messages():
     for message in st.session_state["messages"]:
 
         # 그냥 일반 대화라면
+        # if message.msg_type == "text":
+        #     st.chat_message(message.chat_message.role).write(
+        #         message.chat_message.content
+        #     )
+
         if message.msg_type == "text":
-            st.chat_message(message.chat_message.role).write(
-                message.chat_message.content
-            )
+            with st.chat_message(message.chat_message.role):
+                if message.chat_message.role == "assistant":
+                    st.image(
+                        "https://raw.githubusercontent.com/1020nys/chatbot/main/mindful%20%EC%97%AC%EC%84%B1%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+                        width=50,
+                    )
+                st.write(message.chat_message.content)
+
         # tool을 사용한 경우
         elif message.msg_type == "tool_result":
             # tool이 web_search 이면 아래 format으로 답변
@@ -32,7 +42,6 @@ def print_messages():
                 st.chat_message(message.chat_message.role).write(
                     message.chat_message.content
                 )
-
 
 # add_message도 세가지로 나눠서 답변을 저장하도록 저장
 # 새로운 메시지를 추가
