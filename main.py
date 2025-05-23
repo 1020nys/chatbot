e27710b1fedd02dbf854e1158b6e34d6216c4a93
yeonsoo Noh <122529151+1020nys@users.noreply.github.com>
2025-05-23 19:20:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if user_input:
 
             with col1:
                 st.image(
-                    "woman.png", width=0.8
+                    "woman.png", width=50
                 )
 
             with col2:
